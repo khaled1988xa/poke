561845b4381f25392b6e4e8e9706ddd7567bc572
khaled <kiko1988xaxa@gmail.com>
2024-06-08 13:50:22 +0200

    f

--- vueteil1/src/App.vue
+++ vueteil1/src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-
+import { ref } from 'vue'
+const name = ref('Max Mustermann')
+function changeName() {
+console.log(name.value) // Auslesen
+name.value = 'Erika Musterfrau'
+}
 </script>
 
 <template>
-  
+<div>
+<strong>Dein Name lautet:</strong> {{name}}
+<button type="button" @click="changeName">
+Change name
+</button>
+</div>
 </template>
 
 <style scoped>
