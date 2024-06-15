47a18a8fdd750b8ebad8212c7de706479ef4d5f5
khaled <kiko1988xaxa@gmail.com>
2024-06-15 20:38:20 +0200

    a

--- ubungteil1/src/App.vue
+++ ubungteil1/src/App.vue
@@ -1,95 +1,73 @@
-<script setup>
-
-</script>
-
 <template>
-  <template>
-  <div class="container mt-5">
-    <h1>Übung: Personen Array</h1>
-    <div class="row">
-      <div class="col-md-4">
-        <h2>Person anlegen</h2>
-        <form @submit.prevent="hinzufuegen">
-          <div class="form-group">
-            <label for="vorname">Vorname</label>
-            <input v-model="vorname" type="text" id="vorname" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label for="nachname">Nachname</label>
-            <input v-model="nachname" type="text" id="nachname" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label for="geburtsjahr">Geburtsjahr</label>
-            <input v-model.number="geburtsjahr" type="number" id="geburtsjahr" class="form-control" required>
-          </div>
-          <button type="submit" class="btn btn-success">Hinzufügen</button>
-        </form>
-        <h3>Aktionen</h3>
-        <button @click="letztesElementLoeschen" class="btn btn-warning">Letztes Element löschen</button>
-        <button @click="allesLoeschen" class="btn btn-danger">Alles löschen</button>
+   <div class="container mt-5">
+    <h1>Personen array</h1>
+    <form @submit.prevent="submit">
+      <div class="form-group">
+        <lable  for="vorname">Vorname</lable>
+        <input type="text" id="vorname" v-model="vorname" class="form-control" required>
       </div>
-      <div class="col-md-8">
-        <h2>Personen</h2>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Vorname</th>
-              <th>Nachname</th>
-              <th>Geburtsjahr</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(person, index) in personen" :key="index">
-              <td>{{ person.vorname }}</td>
-              <td>{{ person.nachname }}</td>
-              <td>{{ person.geburtsjahr }}</td>
-            </tr>
-          </tbody>
+      <div class="form-group">
+        <lable  for="nachname">Nachname</lable>
+        <input type="text" id="nachname" v-model="nachname" class="form-control" required>
+      </div>
+      <div class="form-group">
+        <lable  for="geburtsjahr">geburtsjahr</lable>
+        <input type="number" id="geburtsjahr" v-model="geburtsjahr" class="form-control" required>
+      </div>
+      <button type="submit" @click="addperson()" class="btn btn-success">add</button>
+      <button type="submit" @click="deleteall()" class="btn btn-danger" >delet all </button>
+      <button type="submit" @click="deletelast()"  class="btn btn-warning">delete last</button>
+    </form>
+    <table class="table">
+      <thead>
+        <tr>
+          <th >Vorname</th>
+          <th >Nachname</th>
+          <th >Geburtsjahr</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(person,index) in Personen" :key="index">
+        <td>{{person.vorname}}</td>
+        <td>{{person.nachname}}</td>
+        <td>{{person.geburtsjahr}}</td>
+        </tr>
+        </tbody>
         </table>
-      </div>
-    </div>
-  </div>
+   </div>
+
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      personen: [],
-      vorname: '',
-      nachname: '',
-      geburtsjahr: null
-    };
+export default{
+  data(){
+    return{
+      vorname: "",
+      nachname: "",
+      geburtsjahr: "",
+      Personen: []
+    }
   },
-  methods: {
-    hinzufuegen() {
-      if (this.vorname && this.nachname && this.geburtsjahr) {
-        this.personen.push({
-          vorname: this.vorname,
-          nachname: this.nachname,
-          geburtsjahr: this.geburtsjahr
-        });
-        this.vorname = '';
-        this.nachname = '';
-        this.geburtsjahr = null;
+  methods:{
+    addperson(){
+      if(this.vorname &&this.nachname&& this.geburtsjahr !==null){
+        this.Personen.push({vorname: this.vorname, nachname: this.nachname, geburtsjahr: this.geburtsjahr})
+        this.vorname = ""
+        this.nachname = ""
+        this.geburtsjahr = ""
       }
+
     },
-    allesLoeschen() {
-      this.personen = [];
+    deleteall(){
+      this.Personen = []
     },
-    letztesElementLoeschen() {
-      this.personen.pop();
+    deletelast(){
+      this.Personen.pop()
     }
   }
-};
+}
 </script>
 
 <style>
 /* Optional: Eigene Styles hier hinzufügen */
-</style>
-
-</template>
-
-<style scoped>
-
 </style>
