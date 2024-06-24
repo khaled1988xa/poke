c4e87b28a63769b9a30ecb4b0f751ddf3eb9cae2
khaled <kiko1988xaxa@gmail.com>
2024-06-24 13:41:34 +0200

    as

---
+++ counter/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref,defineProps } from 'vue'
+ 
+import CounterDisplay from './components/CounterDisplay.vue'
+import CounterControl from './components/CounterControl.vue'
+
+const counter=ref(0);
+function result(event){
+  counter.value=event.counter;
+  console.log(counter.value);
+}
+ 
+
+</script>
+
+<template>
+  <div class="container">
+    <CounterDisplay :counter="counter" /> <!-- Pass counter value to display component -->
+    <CounterControl @countervalueplus="result" @countervalueminus="result" /> <!-- Handle both increment and decrement events -->
+   
+  </div>
+</template>
+
+<style scoped>
+
+</style>
