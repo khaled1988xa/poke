a40a05631708ce4c3ea2664bb6c63c0f3aed1514
khaled <kiko1988xaxa@gmail.com>
2024-07-02 19:52:51 +0200

    d

--- abschluss-project/src/main.js
+++ abschluss-project/src/main.js
@@ -1,14 +1,29 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue'
+import vuetify from '@/plugins/vuetify'
+import router from '@/router'
+import { loadFonts } from '@/plugins/webfontloader'
+import { createPinia } from 'pinia'
+import axios from 'axios'
 
-const app = createApp(App)
+/**
+ * Interceptors sind Funktionen, die pro Request ausgeführt werden und dazu dienen, den jeweiligen Request zu manipulieren.
+ * In diesem Fall dient er dazu, bei jedem Request den Authentication-Header zu setzen, um auch gegenüber dem Backend eingeloggt zu sein.
+ *
+ * Siehe: https://axios-http.com/docs/interceptors
+ */
+axios.interceptors.request.use(request => {
+    const jwt = localStorage.getItem('jwt')
+    if(!request.headers.hasAuthorization() && jwt) {
+        request.headers.setAuthorization(jwt)
+    }
+    return request
+})
 
-app.use(createPinia())
-app.use(router)
+loadFonts()
+createApp(App)
+    .use(vuetify)
+    .use(createPinia())
+    .use(router)
+    .mount('#app')
 
-app.mount('#app')
--- abschluss-project/src/router/index.js
+++ abschluss-project/src/router/index.js
@@ -1,23 +1,77 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AuthenticationLayout from '@/layouts/AuthenticationLayout.vue'
+import ApplicationLayout from '@/layouts/ApplicationLayout.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import RegistrationView from '@/views/auth/RegistrationView.vue'
+import HomeView from '@/views/HomeView.vue'
+import BasketView from '@/views/BasketView.vue'
+import ProductCreateView from '@/views/ProductCreateView.vue'
+import { useAuthStore } from '@/store/authStore'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes = [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path: '/',
+        component: ApplicationLayout,
+        children: [
+            {
+                path: '',
+                component: HomeView
+            },
+            {
+                path: 'basket',
+                component: BasketView,
+                /**
+                 * "beforeEnter" wird automatisch aufgerufen, wenn der Seitenbesucher auf, in diesem Fall, "/basket" geleitet wird.
+                 * https://router.vuejs.org/guide/advanced/navigation-guards.html#per-route-guard
+                 */
+                beforeEnter() {
+                    const authStore = useAuthStore()
+                    if(!authStore.isUser) {
+                        return '/' // Wenn der Benutzer nicht oder eingeloggt ist oder ein Admin ist, wird er auf die Startseite weitergeleitet
+                    }
+                    return true
+                }
+            },
+            {
+                path: 'new-product',
+                component: ProductCreateView,
+                beforeEnter() {
+                    const authStore = useAuthStore()
+                    if(!authStore.isAdmin) {
+                        return '/' // Wenn der Benutzer nicht eingeloggt ist oder ein Kunde ist, wird er auf die Startseite weitergeleitet
+                    }
+                    return true
+                }
+            }
+        ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+        path: '/login',
+        component: AuthenticationLayout,
+        children: [
+            {
+                path: '',
+                component: LoginView
+            },
+            {
+                path: '/register',
+                component: RegistrationView
+            }
+        ]
     }
-  ]
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
 })
+
+/**
+ * Diese Funktion wird beim initialen Seitenaufruf und bei jedem weiteren Seitenwechsel aufgerufen.
+ * In diesem Fall werden einmal die Benutzerdaten geladen, damit die benutzerabhängigen Anzeigen (Button zum Warenkorb)
+ * und späteren RouteGuards korrekt ausgeführt werden können.
+ * https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
+ */
+router.beforeEach(() => useAuthStore().initialize())
 
 export default router
